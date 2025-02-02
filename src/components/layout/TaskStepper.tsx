e45da4374c8e91d1017ee079bb717969681b6bee
aion0721 <rudolf.aion@gmail.com>
2025-02-02 15:17:12 +0900

    fix: accordion

--- src/components/layout/TaskStepper.tsx
+++ src/components/layout/TaskStepper.tsx
@@ -1,13 +1,30 @@
-import { Box, Button, DataList, HStack, Stack, Status } from "@chakra-ui/react";
+import {
+  AbsoluteCenter,
+  Box,
+  Button,
+  DataList,
+  HStack,
+  Spacer,
+  Stack,
+  Status,
+} from "@chakra-ui/react";
 import TaskSteps from "../TaskSteps";
 import EditDialog from "../EditDialog";
 import { useJobs } from "@/context/JobContext";
 import { JobStatus } from "@/types";
 import { Job } from "@/types";
 import { BiCaretRightCircle, BiCheckCircle } from "react-icons/bi";
+import {
+  AccordionItem,
+  AccordionItemContent,
+  AccordionItemTrigger,
+  AccordionRoot,
+} from "../ui/accordion";
+import { useState } from "react";
 
 const TaskStepper = () => {
   const { jobs, setJobs } = useJobs();
+  const [accordion, setAccordion] = useState<string[]>([]);
 
   const statusColorMap: { [key in JobStatus]?: string } = {
     COMPELETED: "gray",
@@ -32,78 +49,110 @@ const TaskStepper = () => {
   };
 
   return (
-    <Box mt="80px" height="calc(100vh - 80px)" as="main">
-      {jobs.length > 0 ? (
-        jobs.map((job, jobIndex) => (
-          <Box
-            key={job.id}
-            m="20px"
-            py="10px"
-            paddingX="10px"
-            borderWidth="1px"
-            borderColor="border.disabled"
-            bgColor={job.status === JobStatus.COMPLETED ? "gray" : ""}
-            transition="background-color 0.3s ease-in-out" // 背景色のトランジション
-            borderRadius=""
-          >
-            <HStack>
-              <Box w="20%">
-                <DataList.Root gap="1" size="sm">
-                  <DataList.Item>
-                    <DataList.ItemLabel>Status</DataList.ItemLabel>
-                    <DataList.ItemValue>
-                      <Status.Root
-                        size="sm"
-                        colorPalette={
-                          statusColorMap[job?.status as JobStatus] || "red"
-                        }
-                      >
-                        <Status.Indicator />
-                        {job?.status}
-                      </Status.Root>
-                    </DataList.ItemValue>
-                  </DataList.Item>
-                  <DataList.Item>
-                    <DataList.ItemLabel>Name</DataList.ItemLabel>
-                    <DataList.ItemValue>{job?.name}</DataList.ItemValue>
-                  </DataList.Item>
-                  <DataList.Item>
-                    <DataList.ItemLabel>Date</DataList.ItemLabel>
-                    <DataList.ItemValue>
-                      {new Date(job.dueDate).toLocaleDateString()}
-                    </DataList.ItemValue>
-                  </DataList.Item>
-                </DataList.Root>
+    <Box mt="80px" height="calc(100vh - 80px)" as="main" px="20px">
+      <AccordionRoot
+        multiple
+        value={accordion}
+        onValueChange={(e) => setAccordion(e.value)}
+        variant="enclosed"
+      >
+        {jobs.length > 0 ? (
+          jobs.map((job, jobIndex) => (
+            <AccordionItem
+              key={job.id}
+              value={job.id}
+              bgColor={
+                job.status === JobStatus.IN_PROGRESS ? "green.100" : "gray"
+              }
+              transition="background-color 0.3s ease-in-out" // 背景色のトランジション
+            >
+              <Box position="relative">
+                <AccordionItemTrigger>
+                  <Status.Root
+                    colorPalette={
+                      statusColorMap[job?.status as JobStatus] || "red"
+                    }
+                  >
+                    <Status.Indicator />
+                    {job?.status}
+                  </Status.Root>
+                  ジョブ名：{job.name}, 作成日：
+                  {new Date(job.createdAt).toLocaleDateString()}
+                  <Spacer />
+                </AccordionItemTrigger>
               </Box>
-              <TaskSteps jobIndex={jobIndex} w="80%" />
-              <Stack w="20%">
-                <EditDialog job={jobs[jobIndex]} />
-                {job.status === JobStatus.IN_PROGRESS ? (
-                  <Button
-                    colorPalette="green"
-                    onClick={() => handleClose(jobIndex)}
-                    disabled={job.steps !== job.tasks.length}
-                  >
-                    Close
-                    <BiCheckCircle />
-                  </Button>
-                ) : (
-                  <Button
-                    colorPalette="purple"
-                    onClick={() => handleReopen(jobIndex)}
-                    disabled={job.steps !== job.tasks.length}
-                  >
-                    ReOpen
-                    <BiCaretRightCircle />
-                  </Button>
-                )}
-              </Stack>
-            </HStack>
-          </Box>
-        ))
-      ) : (
-        <Box>No tasks</Box>
-      )}
+              <AccordionItemContent>
+                <Box
+                  key={job.id}
+                  py="10px"
+                  paddingX="10px"
+                  borderWidth="1px"
+                  borderColor="border.disabled"
+                  borderRadius=""
+                  bg="white"
+                >
+                  <HStack>
+                    <Box w="20%">
+                      <DataList.Root gap="1" size="sm">
+                        <DataList.Item>
+                          <DataList.ItemLabel>Status</DataList.ItemLabel>
+                          <DataList.ItemValue>
+                            <Status.Root
+                              size="sm"
+                              colorPalette={
+                                statusColorMap[job?.status as JobStatus] ||
+                                "red"
+                              }
+                            >
+                              <Status.Indicator />
+                              {job?.status}
+                            </Status.Root>
+                          </DataList.ItemValue>
+                        </DataList.Item>
+                        <DataList.Item>
+                          <DataList.ItemLabel>Name</DataList.ItemLabel>
+                          <DataList.ItemValue>{job?.name}</DataList.ItemValue>
+                        </DataList.Item>
+                        <DataList.Item>
+                          <DataList.ItemLabel>Date</DataList.ItemLabel>
+                          <DataList.ItemValue>
+                            {new Date(job.dueDate).toLocaleDateString()}
+                          </DataList.ItemValue>
+                        </DataList.Item>
+                      </DataList.Root>
+                    </Box>
+                    <TaskSteps jobIndex={jobIndex} w="80%" />
+                    <Stack w="20%">
+                      <EditDialog job={jobs[jobIndex]} />
+                      {job.status === JobStatus.IN_PROGRESS ? (
+                        <Button
+                          colorPalette="green"
+                          onClick={() => handleClose(jobIndex)}
+                          disabled={job.steps !== job.tasks.length}
+                        >
+                          Close
+                          <BiCheckCircle />
+                        </Button>
+                      ) : (
+                        <Button
+                          colorPalette="purple"
+                          onClick={() => handleReopen(jobIndex)}
+                          disabled={job.steps !== job.tasks.length}
+                        >
+                          ReOpen
+                          <BiCaretRightCircle />
+                        </Button>
+                      )}
+                    </Stack>
+                  </HStack>
+                </Box>
+              </AccordionItemContent>
+            </AccordionItem>
+          ))
+        ) : (
+          <Box>No tasks</Box>
+        )}
+      </AccordionRoot>
     </Box>
   );
 };
